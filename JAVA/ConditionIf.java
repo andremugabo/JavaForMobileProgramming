39c549cdd670cb43ae931870b39b0fe792069674
andremugabo <andremugabo@yahoo.fr>
2023-06-05 19:25:47 +0200

    third commit

---
+++ JAVA/ConditionIf.java
@@ -0,0 +1,10 @@
+public class ConditionIf {
+    public static void main(String Args[]){
+        int x = 30;
+        if (x < 20) {
+            System.out.println("this is If statement ");
+        } else {
+            System.out.println("This is else statement");
+        }
+    }
+}
---
+++ JAVA/IfElse.java
@@ -0,0 +1,20 @@
+public class IfElse {
+    public static void main(String args[]){
+        int x = 30;
+
+        if(x == 10){
+            System.out.println("Value of x is 10");
+        }else if(x > 20){
+            System.out.println("Value of x is > 20");
+
+        }else if( x < 15){
+            System.out.println("Value of x is < 15");
+        }else if(x < 13){
+            System.out.println("Value is < 13");
+        }else if(x > 17){
+            System.out.println("Value of x is > 17");
+        }else{
+            System.out.println("X is not equal to 10");
+        }
+    }
+}
